91660ba2d817f14ae6009d50dfc1c91ba22e84d5
Lukas Was <wetsand@o2.pl>
2020-10-21 22:38:55 +0200

    Updates

---
+++ precache-manifest.795618ff463e6987370b1c77cb9515eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bdfe6a68bc6a4b8306833a85e789b2",
+    "url": "/Clock/index.html"
+  },
+  {
+    "revision": "392f637f01a9a844102f",
+    "url": "/Clock/static/css/main.5b2be0fe.chunk.css"
+  },
+  {
+    "revision": "0a0a86be2ad8f266d117",
+    "url": "/Clock/static/js/2.773d831f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Clock/static/js/2.773d831f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392f637f01a9a844102f",
+    "url": "/Clock/static/js/main.e73991b7.chunk.js"
+  },
+  {
+    "revision": "079c91a92990099e60e6",
+    "url": "/Clock/static/js/runtime-main.d1025cfb.js"
+  }
+]);
